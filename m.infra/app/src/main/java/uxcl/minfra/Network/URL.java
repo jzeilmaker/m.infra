6de84d2b34f6b243edeb3afdfbbdcf6c8f74cb72
Jerry <jjzeilmaker@gmail.com>
2015-12-17 21:15:07 +0100

    URL

---
+++ m.infra/app/src/main/java/uxcl/minfra/Network/URL.java
@@ -0,0 +1,18 @@
+package uxcl.minfra.Network;
+
+/**
+ * Created by Tessa on 17/12/15.
+ */
+public class URL {
+
+    private String BASE = "http://dev.jamaja.nl/";
+
+    final public String PERSON  = BASE + "person.php";
+    final public String RESULT = BASE + "result.php";
+
+
+    public boolean URL () {
+        return true;
+    }
+
+}
